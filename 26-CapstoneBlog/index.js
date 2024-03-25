71a653bb83068e4b8a002e435d17819d639318ed
Gabrielsfg <gabriellfsg00@gmail.com>
2024-03-25 18:05:33 -0300

    Commit

--- 26-CapstoneBlog/index.js
+++ 26-CapstoneBlog/index.js
@@ -5,8 +5,25 @@ const app = express();
 const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//Making the css Dinamic
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", {currentPage: "home"});
+});
+
+app.get("/about", (req, res) => {
+    res.render("about.ejs", {currentPage: "about"});
+});
+
+app.post("/submit", (req,res) => {
+    const postTitle = req.body.pTitle;
+    console.log(postTitle);
+    if(postTitle === ""){
+        res.render("index.ejs", {error:"Post Title must be filled", currentPage: "home"})
+    } else {
+        res.render("create.ejs", {postTitle: postTitle, currentPage: "create"});
+    };
 });
 
 app.listen(port, ()=>{
