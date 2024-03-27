eceeac6e0e25b63bee4e71dc210b79e1ce847a34
gabrielsfg <gabriellfsg00@gmail.com>
2024-03-27 17:39:06 -0300

    Add the create page and the post pages

--- 26-CapstoneBlog/index.js
+++ 26-CapstoneBlog/index.js
@@ -4,29 +4,56 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
+const posts = [];
 
 //Making the css Dinamic
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.render("index.ejs", {currentPage: "home"});
+    res.render("index.ejs", {currentPage: "home", posts: posts});
 });
 
 app.get("/about", (req, res) => {
     res.render("about.ejs", {currentPage: "about"});
 });
 
-app.post("/submit", (req,res) => {
-    const postTitle = req.body.pTitle;
-    console.log(postTitle);
-    if(postTitle === ""){
-        res.render("index.ejs", {error:"Post Title must be filled", currentPage: "home"})
-    } else {
-        res.render("create.ejs", {postTitle: postTitle, currentPage: "create"});
-    };
+app.get("/submit", (req,res) =>{
+    res.render("index.ejs", {currentPage: "home", posts: posts});
+})
+
+app.get("/post/:postTitle", (req,res) =>{
+    const postTitle = req.params.postTitle;
+    const post = posts.find(post => post.title === postTitle);
+    res.render("post.ejs", {currentPage: "post", post:post});
 });
+
+
+
+app.post("/submit", (req,res) => {
+    res.render("create.ejs", {currentPage: "create"});
+});
+
+app.post("/create", (req, res) =>{
+    const postTitle = req.body.postTitle;
+    const postText = req.body.postText;
+    const postAuthor = req.body.postAuthor;
+    const postEmail = req.body.postEmail;
+
+    const newPostItem = new Post(postTitle, postText, postAuthor, postEmail);
+    posts.push(newPostItem);
+    res.redirect("/");
+    console.log(posts);
+})
 
 app.listen(port, ()=>{
     console.log(`Listening on port:${port}`);
 });
 
+class Post {
+    constructor(title, text, author, email){
+        this.title = title;
+        this.text = text;
+        this.author = author;
+        this.email = email;
+    };
+};
